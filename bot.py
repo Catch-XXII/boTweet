8818f246d26463f0a8eb2989ba0e3e89e59b0625
Cüneyd Gültekin Kaya <cgultekink@gmail.com>
2024-07-30 20:16:11 +0300

    minor changes

--- bot.py
+++ bot.py
@@ -1,21 +1,27 @@
-from selenium import webdriver
 import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-class boTweet(object):
+
+class BOTweet(object):
+    _URL = "https://x.com/"
     """
     Twitter bot for sending automatic tweets
     """
 
-    def __init__(self):
-        self.username = input("Please provide a username: ")
-        self.password = input("Please provide a password: ")
-        # self.tweet = input("Please enter your tweet: ")  # you can directly type your tweet or
-        self.tweet = self.text_reader()                    # you can provide a txt file
+    def __init__(self, username, password, tweet):
+        self.username = username
+        self.password = password
+        self.tweet = tweet
+        self.driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install())
+        )
         self.__str__()
         self.connect()
         self.logout()
-
 
     def __str__(self):
         print("Hello ", self.username)
@@ -23,35 +29,47 @@ class boTweet(object):
         print("Sit tight while i am getting things ready for you")
         time.sleep(1)
 
-    def text_reader(self):
-        self.filename = input("Please provide a filename: ")
-        with open(self.filename, "r+", encoding="UTF-8") as file:
-            f = file.read(140)  # Don't exceed the tweet limit
-            return f
-
     def connect(self):
-        self.driver = webdriver.Firefox()
         time.sleep(1)
-        self.driver.get("https://www.twitter.com")
-        time.sleep(1)
-        username = self.driver.find_element_by_name("session[username_or_email]").send_keys(self.username)
-        time.sleep(1)
-        password = self.driver.find_element_by_name("session[password]").send_keys(self.password)
-        time.sleep(1)
-        dologin = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/form/input[1]").click()
-        time.sleep(1)
-        your_message = self.driver.find_element_by_name("tweet").send_keys(self.tweet)
-        time.sleep(1)
-        send_tweet = self.driver.find_element_by_css_selector("form.tweet-form:nth-child(2) > div:nth-child(3) > div:nth-child(2) > button:nth-child(2) > span:nth-child(1)").click()
+        self.driver.get(self._URL)
         time.sleep(1)
 
+        self.driver.find_element(
+            By.XPATH,
+            "(//div[@class='css-146c3p1 r-bcqeeo r-qvutc0 r-37j5jr r-q4m81j r-a023e6 r-rjixqe r-b88u0q r-1awozwy r-6koalj r-18u37iz r-16y2uox r-1777fci'])[3]",
+        ).click()
+
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//input[@name='text']").send_keys(
+            self.username
+        )
+        time.sleep(1)
+        self.driver.find_element(By.NAME, "session[password]").send_keys(self.password)
+        time.sleep(1)
+        self.driver.find_element(
+            By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div[1]/form/input[1]"
+        ).click()
+        time.sleep(1)
+        self.driver.find_element(By.NAME, "tweet").send_keys(self.tweet)
+        time.sleep(1)
+        self.driver.find_element(
+            By.CSS_SELECTOR,
+            "form.tweet-form:nth-child(2) > div:nth-child(3) > "
+            "div:nth-child(2) > button:nth-child(2) > "
+            "span:nth-child(1)",
+        ).click()
+        time.sleep(1)
 
     def logout(self):
-        profile_pic = self.driver.find_element_by_id("user-dropdown-toggle").click()
+        self.driver.find_element(By.ID, "user-dropdown-toggle").click()
         time.sleep(1)
-        do_log_out = self.driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[3]/div/div/div[3]/ul/li[1]/div/ul/li[13]/button").click()
+        self.driver.find_element(
+            By.XPATH,
+            "/html/body/div[2]/div[1]/div[3]/div/div/div[3]/ul/li[1]/div/ul/li[13]/button",
+        ).click()
         time.sleep(2)
         self.driver.quit()
 
 
-
+if __name__ == "__main__":
+    x = BOTweet("trump", "123456", "fuck you!")
